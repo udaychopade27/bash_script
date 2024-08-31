fd3106a60444ab75fb801a129060c2b38fe341f1
udaychopade27 <udaychopade27@gmail.com>
2024-08-31 11:36:57 +0530

    update files

---
+++ if.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Good Morning; Enter your username: "
+read user
+if [ "$user" = "$USER"];
+then
+echo "Hello $user, How are you today?"
+else
+echo "Hello $user, Enter correct username"
+fi
